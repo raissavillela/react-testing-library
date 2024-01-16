9f576229601ed14072b1eea662bd19b6bff4dcba
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-01-16 12:06:01 -0800

    Teste do componente NotFound

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,31 @@
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente Not Found', () => {
+  test('Verifica se a página contém um h2 com o texto Page requested not found', () => {
+    // Arrange
+    renderWithRouter(<App />, { route: '/notfound' });
+
+    // Act
+
+    // Assert
+    expect(
+      screen.getByRole('heading', { name: /page requested not found/i }),
+    ).toBeInTheDocument();
+  });
+
+  test('Verifica se  se a página mostra a imagem com texto alternativo', () => {
+    // Arrange
+    renderWithRouter(<App />, { route: '/notfound' });
+
+    // Act
+
+    // Assert
+    const alternativeText = "Clefairy pushing buttons randomly with text I have no idea what i'm doing";
+
+    expect(screen.getByAltText(alternativeText)).toHaveAttribute('src', '/404.gif');
+  });
+});
+
 test('', () => {});
